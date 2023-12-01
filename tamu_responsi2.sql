495f645a1b732135f988d334c80994dc650dd302
adysepn <adiseptiannugraha12@gmail.com>
2023-12-01 21:38:07 +0700

    nambah api dan sql

---
+++ tamu_responsi2.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Des 2023 pada 14.35
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tamu_responsi2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tamu`
+--
+
+CREATE TABLE `tamu` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tamu`
+--
+
+INSERT INTO `tamu` (`id`, `nama`, `keterangan`) VALUES
+(1, 'fdasfd', 'asdfdsfsadfa'),
+(2, 'asdfw', 'adsfasdfds'),
+(4, 'sdfdsf', 'adsfasdf');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tamu`
+--
+ALTER TABLE `tamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tamu`
+--
+ALTER TABLE `tamu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
